64525485e3768437635570d00c44b1a6c196e477
rfqarbee <rfqarbee@gmail.com>
2024-04-14 23:34:38 +0800

    use nvim for man

--- zsh/.config/zsh/zshenv.zsh
+++ zsh/.config/zsh/zshenv.zsh
@@ -6,6 +6,7 @@ path=($HOME/bin /usr/local/bin $HOME/.local/bin $HOME/.local $path)
 cdpath=(. $HOME $HOME/.config $HOME/.local)
 
 export PATH
+export MANPAGER="nvim +Man!"
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
