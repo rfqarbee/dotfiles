9db97dc6d974f03e86db4302144a4c7a57228acc
Rafiq Arbee <rfqarbee@gmail.com>
2024-04-15 22:18:16 +0800

    export path

--- zsh/.config/zsh/zshenv.zsh
+++ zsh/.config/zsh/zshenv.zsh
@@ -6,14 +6,19 @@ path=($HOME/bin /usr/local/bin $HOME/.local/bin $HOME/.local $path)
 cdpath=(. $HOME $HOME/.config $HOME/.local)
 
 export PATH
-export MANPAGER="nvim +Man!"
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
 export ZDOTDIR=$HOME/.config/zsh
+export XDG_WORK_DIR=$HOME/code
+export XDG_REPOS_DIR=$HOME/repos
+export XDG_DOCUMENTS_DIR=$HOME/Documents
+export XDG_DOWNLOAD_DIR=$HOME/Downloads
+
 #npm
 export npm_config_prefix="$HOME/.local"
 # rust custom path
 export CARGO_HOME=$HOME/.config/cargo
 export RUSTUP_HOME=$HOME/.config/rustup
 
+export MANPAGER="nvim +Man!"
