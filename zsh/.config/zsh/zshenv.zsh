f5aefaad4e7d16b5e0c7254271a70cef6f87eacd
Rafiq Arbee <rfqarbee@gmail.com>
2024-04-21 06:14:58 +0800

    export path

--- zsh/.config/zsh/zshenv.zsh
+++ zsh/.config/zsh/zshenv.zsh
@@ -1,28 +1,34 @@
 #PATH for ZSH
 typeset -U path PATH
 path=($HOME/bin /usr/local/bin $HOME/.local/bin $HOME/.local $path)
-cdpath=(. $HOME/.config $HOME/.local $HOME $HOME/code $HOME/Documents)
+cdpath=(. $HOME/Media/Pictures $HOME/.config $HOME/.local $HOME $HOME/code $HOME/Documents)
 export PATH
 # asdf
 . "$HOME/.config/asdf/asdf.sh"
 fpath=(${ASDF_DIR}/completions $fpath)
 
-# nvidia
-export __GL_SHADER_DISK_CACHE_PATH=$HOME/.cache/nv
-
+# open man in neovim
+export MANPAGER="nvim +Man!"
+# grim
+export GRIM_DEFAULT_DIR=$HOME/Media/Pictures/screenshot
 # w3m
 export W3M_DIR=$HOME/.cache/w3m
 export WWW_HOME="google.com"
-
 # asdf
 export ASDF_DATA_DIR=$HOME/.config/asdf
 
+# xdg
 export ZDOTDIR=$HOME/.config/zsh
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CONFIG_DIRS=/etc/xdg
-export XDG_CACHE_HOME=$HOME/.cache
+export XDG_PICTURES_DIR=$HOME/Media/Pictures/
 export XDG_BIN_HOME=$HOME/.local/bin
 
+# XDG_DATA_DIRS is already defined
+export XDG_CONFIG_DIRS=/etc/xdg
+
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_STATE_HOME=$HOME/.local/state
 # name's shit, wip on it
 export XDG_WORK_DIR=$HOME/code
 export XDG_REPOS_DIR=$HOME/repos
@@ -31,15 +37,13 @@ export XDG_DOWNLOAD_DIR=$HOME/Downloads
 
 # export mariadb hist ; i fucking hate it at $HOME
 export MARIADB_HISTFILE=$HOME/.cache/mariadb_history
-
 # npm
 export npm_config_prefix="$HOME/.local"
 # rust custom path
 export CARGO_HOME=$HOME/.config/cargo
 export RUSTUP_HOME=$HOME/.config/rustup
-
-# open man in neovim
-export MANPAGER="nvim +Man!"
-
 # steam
 export ASDF_PROTONGE_STEAM_COMPAT_DIR=~/.steam/steam/compatibilitytools.d/
+# nvidia
+export __GL_SHADER_DISK_CACHE_PATH=$HOME/.cache/nv
+
