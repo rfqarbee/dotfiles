d180ebbb291e2cf2d7bc01e8b929c888787e0f54
Rafiq Arbee <rfqarbee@gmail.com>
2024-06-05 23:41:54 +0800

    wip on lazydev, dap

---
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,13 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	enabled = false,
+	-- config = function ()
+	--  require("nvim-dap-ui").setup()
+	-- 	require("dap").setup()
+	-- end
+}
