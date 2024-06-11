e5d08cf271ee02a1f96bc450f91df4320d52253c
Rafiq Arbee <rfqarbee@gmail.com>
2024-06-11 20:12:29 +0800

    added obsidian nvim

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -15,11 +15,38 @@ return {
 				name = "notesidian",
 				path = "~/Documents/notesidian",
 			},
-			-- {
-			-- 	name = "work",
-			-- 	path = "~/Documents/notesidian/work",
-			-- },
 		},
-		-- see below for full list of options 👇
+		log_level = vim.log.levels.INFO,
+		notes_subdir = "notes",
+		completion = {
+			-- Set to false to disable completion.
+			nvim_cmp = true,
+			-- Trigger completion at 2 chars.
+			min_chars = 2,
+		},
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>oc"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		preferred_link_style = "wiki",
 	},
 }
