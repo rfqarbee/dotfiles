51a353286f90b0a0319ae39890dcb8ed602c274d
Rafiq Arbee <rfqarbee@gmail.com>
2024-07-21 00:32:48 +0800

    trying out colorschemes

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -3,7 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
-      theme = "tokyonight",
+      -- theme = "tokyonight",
+      theme = "auto",
       -- component_separators = { left = " ", right = "" },
       -- section_separators = { left = "|", right = "|" },
       disabled_filetypes = {
