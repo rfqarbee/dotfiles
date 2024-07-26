e9e6858e0239a3fd8bbeda079468c3349abbbeb7
Rafiq Arbee <rfqarbee@gmail.com>
2024-07-26 23:26:32 +0800

    linting fix and markdown workspaces

--- nvim/.config/nvim/lua/plugins/config/lint.lua
+++ nvim/.config/nvim/lua/plugins/config/lint.lua
@@ -1,33 +1,33 @@
 return {
-	"mfussenegger/nvim-lint",
-	enabled = true,
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local lint = require("lint")
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
-			c = { "cpplint" },
-		}
-		lint.linters.cpplint.args = {
-			"--filter",
-			"-whitespace",
-			"-legal/copyright",
-		}
+  "mfussenegger/nvim-lint",
+  enabled = true,
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+    lint.linters_by_ft = {
+      javascript = { "eslint_d", "eslint" },
+      typescript = { "eslint_d", "eslint" },
+      typescriptreact = { "eslint_d", "eslint" },
+      javascriptreact = { "eslint_d", "eslint" },
+      svelte = { "eslint_d", "eslint" },
+      c = { "cpplint" },
+    }
+    lint.linters.cpplint.args = {
+      "--filter",
+      "-whitespace",
+      "-legal/copyright",
+    }
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = vim.api.nvim_create_augroup("LintGroup", { clear = true }),
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      group = vim.api.nvim_create_augroup("LintGroup", { clear = true }),
+      callback = function()
+        lint.try_lint()
+      end,
+    })
 
-		-- manually call lint
-		vim.keymap.set("n", "<leader>ll", function()
-			lint.try_lint()
-		end, { desc = "Linting" })
-	end,
+    -- manually call lint
+    vim.keymap.set("n", "<leader>ll", function()
+      lint.try_lint()
+    end, { desc = "Linting" })
+  end,
 }
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -41,83 +41,205 @@ return {
     },
     lazy = true,
     ft = "markdown",
-    cond = function()
-      local getos = os.getenv("OS")
-      local windowPath = "C:\\Users\\muhammadrafiq\\Documents\\Vaults"
-      local defaultOS = "/home/rafiq/Documents/Vaults"
+    -- opts = {
+    --   workspaces = {
+    --     {
+    --       name = "notesidian",
+    --       path = "~/Documents/Vaults",
+    --       overrides = {
+    --         daily_notes = {
+    --           folder = "1. Quicknotes",
+    --           date_format = "%d-%m-%Y",
+    --           alias_format = "%B %-d,%Y",
+    --           default_tags = { "quicknotes" },
+    --         },
+    --       },
+    --     },
+    --     {
+    --       name = "test",
+    --       path = "~/Documents/test",
+    --       overrides = {
+    --         notes_subdir = "notes",
+    --         daily_notes = {
+    --           folder = "notes/dailes",
+    --           date_format = "%d-%m-%Y",
+    --           alias_format = "%B %-d,%Y",
+    --           default_tags = { "quicknotes" },
+    --         },
+    --       },
+    --     },
+    --   },
+    --   note_id_func = function(title)
+    --     local suffix = ""
+    --     if title ~= nil then
+    --       suffix = title
+    --     else
+    --       for _ = 1, 4 do
+    --         suffix = suffix .. string.char(math.random(65, 90))
+    --       end
+    --     end
+    --
+    --     return suffix
+    --   end,
+    --
+    --   log_level = vim.log.levels.INFO,
+    --   templates = {
+    --     folder = "Templates",
+    --     date_format = "%d/%m/%Y",
+    --     time_format = "%H:%M",
+    --   },
+    --   ui = {
+    --     enable = false,
+    --     checkboxes = {
+    --       [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+    --       ["x"] = { char = "", hl_group = "ObsidianDone" },
+    --       ["/"] = { char = "󰥔", hl_group = "ObsidianRightArrow" },
+    --       [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+    --       ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+    --       ["!"] = { char = "", hl_group = "ObsidianImportant" },
+    --     },
+    --   },
+    --   preferred_link_style = "wiki",
+    --   mappings = {
+    --     ["gf"] = {
+    --       action = function()
+    --         return require("obsidian").util.gf_passthrough()
+    --       end,
+    --       opts = { desc = "GF passthru", noremap = false, expr = true, buffer = true },
+    --     },
+    --     ["<CR>"] = {
+    --       action = function()
+    --         return require("obsidian").util.smart_action()
+    --       end,
+    --       opts = { desc = "Smart action", expr = true, buffer = true },
+    --     },
+    --     ["<leader>oc"] = {
+    --       action = function()
+    --         return require("obsidian").util.toggle_checkbox()
+    --       end,
+    --       opts = { desc = "Toggle Checkbox (obs)", buffer = true },
+    --     },
+    --     ["<leader>on"] = {
+    --       action = "<cmd>ObsidianDailies<cr>",
+    --       opts = { desc = "Quick notes" },
+    --     },
+    --     ["<leader>od"] = {
+    --       action = "<cmd>ObsidianToday +1<cr>",
+    --       opts = { desc = "Notes Tomorrow" },
+    --     },
+    --     ["<leader>ox"] = {
+    --       action = "<cmd>ObsidianTags<cr>",
+    --       opts = { desc = "Obsidian Tags", buffer = true },
+    --     },
+    --     ["<leader>ot"] = { action = "<cmd>ObsidianTemplate<cr>", opts = { desc = "Template", buffer = true } },
+    --   },
+    -- },
+    config = function()
+      require("obsidian").setup({
+        workspaces = {
+          {
+            name = "notesidian",
+            path = "~/Documents/Vaults",
+            overrides = {
+              daily_notes = {
+                folder = "1. Quicknotes",
+                date_format = "%d-%m-%Y",
+                alias_format = "%B %-d,%Y",
+                default_tags = { "quicknotes" },
+              },
+            },
+          },
+          {
+            name = "test",
+            path = "~/Documents/test",
+            overrides = {
+              notes_subdir = "notes",
+              daily_notes = {
+                folder = "notes/dailes",
+                date_format = "%d-%m-%Y",
+                alias_format = "%B %-d,%Y",
+                default_tags = { "quicknotes" },
+              },
+            },
+          },
+        },
+        note_id_func = function(title)
+          local suffix = ""
+          if title ~= nil then
+            suffix = title
+          else
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
+          end
 
-      if getos ~= nil and string.find(getos:lower(), "windows") then
-        if vim.fn.getcwd() == windowPath then
-          return true
-        end
-      else
-        if vim.fn.getcwd() == defaultOS then
-          return true
-        end
-      end
+          return suffix
+        end,
+
+        log_level = vim.log.levels.INFO,
+        templates = {
+          folder = "Templates",
+          date_format = "%d/%m/%Y",
+          time_format = "%H:%M",
+        },
+        ui = {
+          enable = false,
+          checkboxes = {
+            [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "", hl_group = "ObsidianDone" },
+            ["/"] = { char = "󰥔", hl_group = "ObsidianRightArrow" },
+            [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+            ["!"] = { char = "", hl_group = "ObsidianImportant" },
+          },
+        },
+        preferred_link_style = "wiki",
+        mappings = {
+          ["gf"] = {
+            action = function()
+              return require("obsidian").util.gf_passthrough()
+            end,
+            opts = { desc = "GF passthru", noremap = false, expr = true, buffer = true },
+          },
+          ["<CR>"] = {
+            action = function()
+              return require("obsidian").util.smart_action()
+            end,
+            opts = { desc = "Smart action", expr = true, buffer = true },
+          },
+          ["<leader>oc"] = {
+            action = function()
+              return require("obsidian").util.toggle_checkbox()
+            end,
+            opts = { desc = "Toggle Checkbox (obs)", buffer = true },
+          },
+          ["<leader>on"] = {
+            action = "<cmd>ObsidianDailies<cr>",
+            opts = { desc = "Quick notes" },
+          },
+          ["<leader>od"] = {
+            action = "<cmd>ObsidianToday +1<cr>",
+            opts = { desc = "Notes Tomorrow" },
+          },
+          ["<leader>ox"] = {
+            action = "<cmd>ObsidianTags<cr>",
+            opts = { desc = "Obsidian Tags", buffer = true },
+          },
+          ["<leader>ot"] = { action = "<cmd>ObsidianTemplate<cr>", opts = { desc = "Template", buffer = true } },
+          ["<leader>ov"] = {
+            action = "<cmd>ObsidianBacklinks vsplit<cr>",
+            opts = { desc = "ObsidianBacklinks", buffer = true },
+          },
+        },
+      })
+      vim.keymap.set("v", "<leader>ol", ":ObsidianLinkNew<cr>", { desc = "Create obsidian link", buffer = true })
+      vim.keymap.set(
+        "v",
+        "<leader>oe",
+        ":ObsidianExtractNote<cr>",
+        { desc = "Extract note and create links", buffer = true }
+      )
     end,
-    opts = {
-      workspaces = {
-        { name = "notesidian", path = "~/Documents/Vaults" },
-      },
-      daily_notes = {
-        folder = "1. Quicknotes",
-        date_format = "%B %-d,%Y",
-        alias_format = "%d-%m-%Y",
-        default_tags = { "quicknotes" },
-      },
-      log_level = vim.log.levels.INFO,
-      templates = {
-        folder = "~/Documents/Vaults/Templates",
-        date_format = "%d/%m/%Y",
-        time_format = "%H:%M",
-      },
-      ui = {
-        enable = false,
-        checkboxes = {
-          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-          ["x"] = { char = "", hl_group = "ObsidianDone" },
-          ["/"] = { char = "󰥔", hl_group = "ObsidianRightArrow" },
-          [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-          ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-          ["!"] = { char = "", hl_group = "ObsidianImportant" },
-        },
-      },
-      preferred_link_style = "wiki",
-      mappings = {
-        ["gf"] = {
-          action = function()
-            return require("obsidian").util.gf_passthrough()
-          end,
-          opts = { desc = "GF passthru", noremap = false, expr = true, buffer = true },
-        },
-        ["<CR>"] = {
-          action = function()
-            return require("obsidian").util.smart_action()
-          end,
-          opts = { desc = "Smart action", expr = true, buffer = true },
-        },
-        ["<leader>oc"] = {
-          action = function()
-            return require("obsidian").util.toggle_checkbox()
-          end,
-          opts = { desc = "Toggle Checkbox (obs)", buffer = true },
-        },
-        ["<leader>on"] = {
-          action = "<cmd>ObsidianDailies<cr>",
-          opts = { desc = "Quick notes" },
-        },
-        ["<leader>od"] = {
-          action = "<cmd>ObsidianTomorrow<cr>",
-          opts = { desc = "Notes Tomorrow" },
-        },
-        ["<leader>ox"] = {
-          action = "<cmd>ObsidianTags<cr>",
-          opts = { desc = "Obsidian Tags" },
-        },
-        ["<leader>ot"] = { action = "<cmd>ObsidianTemplate<cr>", opts = { desc = "Template", buffer = true } },
-      },
-    },
   },
 
   -- neotree (only load in Vaults)
