9cf3b4e9737284c48e406fc449bdf8880232f8a5
Rafiq Arbee <rfqarbee@gmail.com>
2024-08-24 00:01:31 +0800

    doing my own colorscheme

--- nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
     "rafamadriz/neon",
+    enabled = true,
     config = function()
       vim.g.neon_style = "doom"
       vim.g.neon_italic_keyword = false
@@ -8,7 +9,7 @@ return {
       vim.g.neon_italic_variable = false
       vim.g.neon_bold = true
       vim.g.neon_transparent = false
-      vim.cmd("colorscheme neon")
+      -- vim.cmd("colorscheme neon")
     end,
   },
   {
