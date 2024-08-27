39a7503d85a434f96a5766fdd68b7edc90155afb
Rafiq Arbee <rfqarbee@gmail.com>
2024-08-27 18:55:46 +0800

    windows 11 nice :)

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "gs", "<cmd>Git | resize 32<cr>", { silent = true, desc = "Git status" })
+    vim.keymap.set("n", "gs", "<cmd>Git<cr>", { silent = true, desc = "Git status" })
     vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<cr>", { silent = true, desc = "Git status" })
     vim.keymap.set("n", "<leader>gl", "<cmd>Gllog<cr>", { silent = true, desc = "Git status" })
   end,
