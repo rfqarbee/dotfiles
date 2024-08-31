15a326a24754de50dc777339f3ee70810cc78d7b
Rafiq Arbee <rfqarbee@gmail.com>
2024-09-01 00:06:55 +0800

    dadbod

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -13,8 +13,7 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    local map = require("utils.mapkey")
+    map("n", "<leader>do", "<cmd>DBUIToggle<cr>")
   end,
-  keys = {
-    { "<leader>do", "<cmd>DBUIToggle<cr>" },
-  },
 }
