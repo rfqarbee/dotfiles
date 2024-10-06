96c9b5e92fec059484aae5be781c0e37fc99a4bd
Rafiq Arbee <rfqarbee@gmail.com>
2024-10-07 02:50:19 +0800

    i wont touch anymore i swear

--- nvim/.config/nvim/plugin/autocmd.lua
+++ nvim/.config/nvim/plugin/autocmd.lua
@@ -6,28 +6,6 @@ local SaveAll = vim.api.nvim_create_augroup("SaveAll", { clear = true })
 local resizeWin = vim.api.nvim_create_augroup("resizeWin", { clear = true })
 local Quickfix = vim.api.nvim_create_augroup("Quickfix", { clear = true })
 
--- autocmd("CmdlineLeave", {
---   desc = "Gcommit",
---   group = vim.api.nvim_create_augroup("gcommit", { clear = true }),
---   callback = function()
---     local ft = vim.bo.filetype
---     if ft == "gitcommit" then
---       -- ("inside?")
---       -- vim.cmd("above Git commit")
---     end
---   end,
--- })
-
--- autocmd("BufEnter", {
---   desc = "Resize Gstatus",
---   group = vim.api.nvim_create_augroup("resizeFugitive", { clear = true }),
---   callback = function()
---     local ft = vim.bo.filetype
---     if ft == "fugitive" then
---       vim.cmd("resize 32")
---     end
---   end,
--- })
 
 autocmd("BufWritePre", {
   desc = "Delete trailing whitespace",
@@ -73,54 +51,55 @@ usercmd("ToggleDiagnostics", function()
   end
 end, {})
 
--- autocmd("BufWritePost", {
---   desc = "Save modified files",
---   group = SaveAll,
---   pattern = "*",
---   callback = function()
---     local buflist = {}
---     local bufnum = {}
---     local buffer = vim.api.nvim_cmd({ cmd = "buffers" }, { output = true })
---     local function getstr(fullpath)
---       local segments = {}
---       for segment in string.gmatch(fullpath, "[^/]+") do
---         table.insert(segments, segment)
---       end
---
---       if #segments >= 2 then
---         return segments[#segments - 1] .. "/" .. segments[#segments]
---       elseif #segments == 1 then
---         return segments[1]
---       else
---         return nil
---       end
---     end
---     -- get all line
---     for line in buffer:gmatch("[^\r\n]+") do
---       table.insert(buflist, line)
---     end
---     -- only store buffer number
---     for _, line in ipairs(buflist) do
---       local getNum = line:match("^%s*(%d+)")
---       if getNum then
---         table.insert(bufnum, getNum)
---       end
---     end
---     -- save for buffers that has changes and notify
---     for _, bufnr in ipairs(bufnum) do
---       if vim.api.nvim_get_option_value("modified", { buf = tonumber(bufnr) }) then
---         local savedFiles = vim.api.nvim_buf_get_name(tonumber(bufnr))
---         vim.notify(getstr(savedFiles), vim.log.levels.INFO, {
---           title = "File saved!",
---           timeout = 3500,
---         })
---         vim.api.nvim_buf_call(tonumber(bufnr), function()
---           vim.cmd("silent write")
---         end)
---       end
---     end
---   end,
--- })
+-- TODO: rewrite this ass 
+autocmd("BufWritePost", {
+  desc = "Save modified files",
+  group = SaveAll,
+  pattern = "*",
+  callback = function()
+    local buflist = {}
+    local bufnum = {}
+    local buffer = vim.api.nvim_cmd({ cmd = "buffers" }, { output = true })
+    local function getstr(fullpath)
+      local segments = {}
+      for segment in string.gmatch(fullpath, "[^/]+") do
+        table.insert(segments, segment)
+      end
+
+      if #segments >= 2 then
+        return segments[#segments - 1] .. "/" .. segments[#segments]
+      elseif #segments == 1 then
+        return segments[1]
+      else
+        return nil
+      end
+    end
+    -- get all line
+    for line in buffer:gmatch("[^\r\n]+") do
+      table.insert(buflist, line)
+    end
+    -- only store buffer number
+    for _, line in ipairs(buflist) do
+      local getNum = line:match("^%s*(%d+)")
+      if getNum then
+        table.insert(bufnum, getNum)
+      end
+    end
+    -- save for buffers that has changes and notify
+    for _, bufnr in ipairs(bufnum) do
+      if vim.api.nvim_get_option_value("modified", { buf = tonumber(bufnr) }) then
+        local savedFiles = vim.api.nvim_buf_get_name(tonumber(bufnr))
+        vim.notify(getstr(savedFiles), vim.log.levels.INFO, {
+          title = "File saved!",
+          timeout = 3500,
+        })
+        vim.api.nvim_buf_call(tonumber(bufnr), function()
+          vim.cmd("silent write")
+        end)
+      end
+    end
+  end,
+})
 
 autocmd("QuickFixCmdPost", {
   group = Quickfix,
