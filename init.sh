7822daefb7235b7f258aed3a889d2d4a4fdbbad1
Rafiq Arbee <rfqarbee@gmail.com>
2025-03-17 00:41:55 +0800

    install script

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 scripts=$HOME/.config/hypr/scripts
+logs="./_logs/logs"
 
 init_install() {
     extras=()
@@ -7,14 +8,36 @@ init_install() {
     while read -r out; do
 	extras+=($out)
     done < ./extras.txt
-    while read -r out; do
-	aur+=($out)
-    done < ./aur.txt
-    sudo pacman -Sp ${extras[@]}
-
+    if [[ ${#extras} -gt 0 ]]; then
+	yes | sudo pacman -Sp ${extras[@]}
+    fi
 }
 
-pacman () {
+# no checking since installing it barebone
+init_daemon(){
+    # ssd
+    sudo systemctl enable --now fstrim.service 2>&1 | tee -a $logs
+    sudo systemctl enable --now fstrim.timer 2>&1 | tee -a $logs
+    #bluetooth
+    sudo systemctl enable --now bluetooth.service 2>&1 | tee -a $logs
+    # audio
+    systemctl --user enable --now pipewire.socket pipewire-pulse.socket wireplumber.service 2>&1 | tee -a $logs
+    systemctl --user enable --now pipewire.servce 2>&1 | tee -a $logs
+    # sddm
+    sudo systemctl enable sddm 2>&1 | tee -a $logs
+    # firewall
+    # futher can be read https://wiki.archlinux.org/title/Uncomplicated_Firewall
+    sudo systemctl enable --now ufw.service 2>&1 | tee -a $logs
+    sudo ufw default deny
+    sudo ufw allow 192.168.0.0/24 # allow LAN
+    sudo ufw allow Deluge # bittorrent
+    sudo ufw limit ssh
+    #network
+    sudo systemctl enable --now NetworkManager.service 2>&1 | tee -a $logs
+}
+
+pacman() {
+    pacman_conf="/etc/pacman.conf"
     opts=(
 	"Color"
 	"CheckSpace"
@@ -22,30 +45,23 @@ pacman () {
 	"ParallelDownloads"
     )
 
-for line in "${opts[@]}"; do
-    if grep -q "^#$line" "$pacman_conf"; then
-        sudo sed -i "s/^#$line/$line/" "$pacman_conf"
-        echo -e "Uncommented: $line ${RESET}" 2>&1 | tee -a "$LOG"
+    for line in "${opts[@]}"; do
+	if grep -q "^#$line" "$pacman_conf"; then
+	    sudo sed -i "s/^#$line/$line/" "$pacman_conf"
+	    echo -e "Toggle $line" | tee -a "$log"
+	else
+	    echo -e "$line already toggled." | tee -a "$log"
+	fi
+    done
+
+    if grep -q "^ParallelDownloads" "$pacman_conf" && ! grep -q "^ILoveCandy" "$pacman_conf"; then
+	sudo sed -i "/^ParallelDownloads/a ILoveCandy" "$pacman_conf"
+	echo -e "Parellel downloads enable; Ilovecandy added" | tee -a "$log"
     else
-        echo -e "$line is already uncommented. ${RESET}" 2>&1 | tee -a "$LOG"
+	echo -e "Already set, skip" | tee -a "$log"
     fi
-done
-
-# Add "ILoveCandy" below ParallelDownloads if it doesn't exist
-if grep -q "^ParallelDownloads" "$pacman_conf" && ! grep -q "^ILoveCandy" "$pacman_conf"; then
-    sudo sed -i "/^ParallelDownloads/a ILoveCandy" "$pacman_conf"
-    echo -e "${CAT} Added ${MAGENTA}ILoveCandy${RESET} after ${MAGENTA}ParallelDownloads${RESET}. ${RESET}" 2>&1 | tee -a "$LOG"
-else
-    echo -e "${CAT} It seems ${YELLOW}ILoveCandy${RESET} already exists ${RESET} moving on.." 2>&1 | tee -a "$LOG"
-fi
-
-echo -e "${CAT} ${MAGENTA}Pacman.conf${RESET} spicing up completed ${RESET}" 2>&1 | tee -a "$LOG"
-
-
-# updating pacman.conf
-printf "\n%s - ${SKY_BLUE}Synchronizing Pacman Repo${RESET}\n" "${INFO}"
-sudo pacman -Sy
-
+	echo "synching pacman..."
+	sudo pacman -Syu
 }
 
 init_hypr() {
@@ -59,7 +75,6 @@ init_hypr() {
     swww="swww img"
     effect="--transition-bezier .43,1.19,1,.4 --transition-fps 30 --transition-type grow --transition-pos 0.925,0.977 --transition-duration 2"
 
-
     gsettings set org.gnome.desktop.interface color-scheme $color_scheme > /dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme > /dev/null 2>&1 &
     gsettings set org.gnome.desktop.interface icon-theme $icon_theme > /dev/null 2>&1 &
@@ -68,14 +83,14 @@ init_hypr() {
     kvantummanager --set "$kvantum_theme" > /dev/null 2>&1 &
 
 	if [ -f "$wallpaper" ]; then
-		wallust run -s $wallpaper > /dev/null
-		swww query || swww-daemon && $swww $wallpaper $effect
-	    "$scripts/wallust.sh" > /dev/null 2>&1 &
+	    wallust run -s $wallpaper > /dev/null
+	    swww query || swww-daemon && $swww $wallpaper $effect
+	    "$scripts/wallust.sh" 2>&1  | tee -a $logs &
 	fi
 
 	if [ -d "$HOME/.config/waybar/config" ]; then
-       	ln -sf "$waybar" "$HOME/.config/waybar/style.css"
-      	"$scripts/refresh_daemon.sh --all" > /dev/null 2>&1 &
+	    ln -sf "$waybar" "$HOME/.config/waybar/style.css"
+	    "$scripts/refresh_daemon.sh --all" 2>&1 | tee -a $logs &
 	fi
 }
 
@@ -95,11 +110,37 @@ init_dir() {
 }
 
 init_yay() {
+    sudo pacman -S yay-bin
     if sudo pacman -Qs yay-bin >/dev/null; then
 	yay -Y --gendb
 	yay -Syu --devel
+	while read -r out; do
+	    aur+=($out)
+	done < ./aur.txt
+	if [ $(command -v yay) ];then
+	    if [[ ${#aur} -gt 0 ]]; then
+		yes | yay -S ${aur[@]}
+	    fi
+	fi
     else
 	echo "Install yay-bin or build from souce"
     fi
 }
+
+echo "Pacman conf"
+pacman
+sleep 2
+echo "Init Yay"
+init_yay
+sleep 2
+echo "Install packages"
+init_intall
+sleep 2
+echo "Init services"
+init_daemon
+sleep 2
+echo "Creating directories and init git"
+init_dir
+echo "Init hyprland defaults"
+init_hypr
 
--- install.sh
+++ install.sh
@@ -1,37 +1,48 @@
 #!/usr/bin/env bash
 
-echo "$PWD"
-root=$PWD
-stow_dirs=$(find $PWD -mindepth 1 -maxdepth 1 -not -path "*/.git" -type d)
-term="kitty"
-prompt="ohmyposh"
-ignore=""
-touch log
 
-for folders in $stow_dirs; do
-    currentFolder=$(basename $folders)
+# echo "$PWD"
+# root=$PWD
+# stow_dirs=$(find $PWD -mindepth 1 -maxdepth 1 -not -path "*/.git" -type d)
+# term="kitty"
+# prompt="ohmyposh"
+# ignore=""
+#
+if [ ! $(find "_logs" -type d) ]; then
+    mkdir _logs
+    touch _logs/logs
+fi
 
-    if [[ $currentFolder == 'term' ]]; then
-        term_folder=$(find $currentFolder -mindepth 2 -maxdepth 2)
-        for folder in $term_folder; do
-            ignore_folder=$(basename $folder)
-            if [[ $ignore_folder != $term && $ignore_folder != $prompt ]]; then
-                ignore="$ignore${ignore_folder}|"
-            fi
-        done
-        ignore=$(echo $ignore | sed 's/|$//')
-        echo "Ignoring $(echo $ignore | sed 's/|/ /g')"
-        echo -e "Stowing $currentFolder"
-        stow $currentFolder --ignore "$ignore"
-    else
-        echo -e "Stowing $currentFolder"
-        stow $currentFolder
-    fi
-done
 
-cd $root
-echo -e "\nInit submodule"
-git submodule update --init --recursive --remote
-git submodule foreach git checkout main
+echo $logs
 
-echo "Done"
+"./init.sh"
+#
+#
+# for folders in $stow_dirs; do
+#     currentFolder=$(basename $folders)
+#
+#     if [[ $currentFolder == 'term' ]]; then
+#         term_folder=$(find $currentFolder -mindepth 2 -maxdepth 2)
+#         for folder in $term_folder; do
+#             ignore_folder=$(basename $folder)
+#             if [[ $ignore_folder != $term && $ignore_folder != $prompt ]]; then
+#                 ignore="$ignore${ignore_folder}|"
+#             fi
+#         done
+#         ignore=$(echo $ignore | sed 's/|$//')
+#         echo "Ignoring $(echo $ignore | sed 's/|/ /g')"
+#         echo -e "Stowing $currentFolder"
+#         stow $currentFolder --ignore "$ignore"
+#     else
+#         echo -e "Stowing $currentFolder"
+#         stow $currentFolder
+#     fi
+# done
+#
+# cd $root
+# echo -e "\nInit submodule"
+# git submodule update --init --recursive --remote
+# git submodule foreach git checkout main
+#
+# echo "Done"
